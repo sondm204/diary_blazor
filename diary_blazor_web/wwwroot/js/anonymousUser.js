79b6c7bde78c47a9f420546ec95f37233641c337
sondmfpt <sondmhe186291@fpt.edu.vn>
2025-07-20 21:50:24 +0700

    fix

--- diary_blazor_web/wwwroot/js/anonymousUser.js
+++ diary_blazor_web/wwwroot/js/anonymousUser.js
@@ -1,9 +1,8 @@
 ﻿window.createOrGetAnonymousUser = function () {
     let userId = localStorage.getItem("anonymousUserId");
 
-    // Nếu không có userId, tạo một ID mới và lưu vào localStorage
     if (!userId) {
-        userId = 'user-' + Math.random().toString(36).substr(2, 9); // Tạo ID ngẫu nhiên
+        userId = 'user-' + Math.random().toString(36).substr(2, 9);
         localStorage.setItem("anonymousUserId", userId);
     }
 
